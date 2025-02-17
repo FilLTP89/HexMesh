a6094074e1ef167ff12c4ab8216b25b5e1262e0c
Filippo Gatti <filippo.gatti@centralesupelec.fr>
2025-02-17 12:26:26 +0100

    [BUGFIX] moving nodes header

--- include/hexa.h
+++ include/hexa.h
@@ -428,7 +428,7 @@ void GetMeshFromSurface(hexa_tree_t *tree, const char *surface_topo, std::vector
 void GetInterceptedElements(hexa_tree_t *mesh, std::vector<double> &coords, std::vector<int> &elements_ids, const char *surface_bathy);
 void Apply_material(hexa_tree_t *mesh, std::vector<double> &coords, const char *surface_bathy);
 void ExtrudePMLElements(hexa_tree_t *mesh, std::vector<double> &coords);
-void MovingNodes(hexa_tree_t *mesh, std::vector<double> &coords, std::vector<int> &nodes_b_mat, const char *surface);
+void MovingNodes(hexa_tree_t *mesh, std::vector<double> &coords, std::vector<int> &nodes_b_mat);
 void MeshOpt(hexa_tree_t *mesh, std::vector<double> &coords, std::vector<int> material_fixed_nodes);
 void PillowingInterface(hexa_tree_t *mesh, std::vector<double> &coords, std::vector<int> &nodes_b_mat);
 void Adjust_material(hexa_tree_t *mesh);
